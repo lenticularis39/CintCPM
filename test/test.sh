73427f01201218d5751d9b9c1eebaf9c8e71422f
Tomas Glozar <tglozar@gmail.com>
2024-12-31 22:13:14 +0100

    Update test
    Add blank 160k floppy image and modify script to copy
    hello.cmd onto a secondary floppy drive.
    
    This is necessary since cpmtools 320k floppy support is
    broken.

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 rm -f .test.img
 cp cpm86_11_pc_xt.img .test.img
-../cpmtools/cpmcp -f ibmpc-514ds .test.img ../hello/hello.cmd 0:hello.cmd
-qemu-system-i386 -blockdev driver=file,node-name=f0,filename=.test.img \
-	-device floppy,drive=f0 -no-fd-bootchk
+cp blank_ibm_pc_ss_160k.img .test2.img
+../cpmtools/cpmcp -f ibmpc-514ss .test2.img ../hello/hello.cmd 0:hello.cmd
+qemu-system-i386 -no-fd-bootchk \
+	-blockdev driver=file,node-name=f0,filename=.test.img \
+	-device floppy,drive=f0 \
+	-blockdev driver=file,node-name=f1,filename=.test2.img \
+	-device floppy,drive=f1
