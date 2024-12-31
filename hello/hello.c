700d54afeae6004dd4a9a278b728543a95bd6961
Tomas Glozar <tglozar@gmail.com>
2024-12-31 23:42:59 +0100

    Better Hello world
    Actually print out "Hello world" using BDOS 0x02 "CONSOLE OUTPUT"
    in a loop.

--- hello/hello.c
+++ hello/hello.c
@@ -1,5 +1,24 @@
+void print_char(char c);
+void print_string(char *s);
+void exit_prog();
+
 void _start() {
-	for (int i = 0; i < 1000000; i++);
-	asm("mov $0, %ecx");
+	print_string("Hello world from GCC!");
+	exit_prog();
+}
+
+void print_char(char c) {
+	asm("mov $0x02, %cl");
+	asm("mov %0, %%dl" : : "r"(c) :);
+	asm("int $0xe0");
+}
+
+void print_string(char *s) {
+	while (*s != 0)
+		print_char(*s++);
+}
+
+void exit_prog() {
+	asm("mov $0, %cl");
 	asm("int $0xe0");
 }
