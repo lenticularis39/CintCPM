0405ccaaf0439d21bf555bdc3fa04dcb9266f41c
Tomas Glozar <tglozar@gmail.com>
2025-01-01 13:01:39 +0100

    init: Add stack setup
    Per CP/M-86 documentation, all programs doing BDOS calls require
    a local stack.

--- libcpm/init.c
+++ libcpm/init.c
@@ -1,6 +1,16 @@
+#define STACK_LEN 256
+
 void main();
 
+char stack[STACK_LEN];
+
 void _start() {
+	asm volatile(
+	"mov %0, %%esp\n\t"
+	"mov %%cs, %%bx\n\t"
+	"mov %%bx, %%ss"
+	: : "r"(stack + STACK_LEN - 1)
+	: "%bx");
 	main();
 	asm volatile(
 	"mov $0, %dl\n\t"
