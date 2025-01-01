f1d0f9c31c744ba2f01775db92024a56415a8dd3
Tomas Glozar <tglozar@gmail.com>
2025-01-01 19:15:18 +0100

    test: Allow running more programs
    test.sh is renamed to run.sh and a parameter is added to run other
    programs than hello.

--- test/run.sh
+++ test/run.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+set -e
+[ -z "$1" ] && PROG=hello || PROG=$1
 rm -f .test.img
 cp cpm86_11_pc_xt.img .test.img
 cp blank_ibm_pc_ss_160k.img .test2.img
-../cpmtools/cpmcp -f ibmpc-514ss .test2.img ../hello/hello.cmd 0:hello.cmd
+../cpmtools/cpmcp -f ibmpc-514ss .test2.img ../$PROG/$PROG.cmd 0:$PROG.cmd
 qemu-system-i386 -no-fd-bootchk \
 	-blockdev driver=file,node-name=f0,filename=.test.img \
 	-device floppy,drive=f0 \
